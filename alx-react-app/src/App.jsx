f445da1f73ca43defb8f9448bf9a93e475ac3392
dwin504 <dwintez254@gmail.com>
2025-08-03 02:43:57 +0300

    added:    alx-react-app/src/components/Footer.jsx

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -3,8 +3,8 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import WelcomeMessage from './components/WelcomeMessage';
 import Header from './components/Header';
-import MainContent from './components/MainContent';
-import Footer from './components/Footer';
+import MainContent from './components/Maincontent';
+
 
 
 function App() {
@@ -24,7 +24,6 @@ function App() {
       <WelcomeMessage />
       <Header/>
       <MainContent/>
-      <Footer />
 
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
