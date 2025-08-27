b634c00922e08edd921609724b37043e37574af6
dwin504 <dwintez254@gmail.com>
2025-08-28 02:38:51 +0300

    added recipe-sharing-app project

--- recipe-sharing-app/src/App.jsx
+++ recipe-sharing-app/src/App.jsx
@@ -1,20 +1,10 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import WelcomeMessage from './components/WelcomeMessage';
-import Header from './components/Header';
-import MainContent from './components/Maincontent';
-import Footer from './components/Footer';
-import UserProfile from './components/UserProfile';
-import ProfilePage from './components/ProfilePage'; 
-import { UserContext } from './components/UserContext';
-
-
+import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
-  const userData = { name: "Jane Doe", email: "jane.doe@example.com" };
-
 
   return (
     <>
@@ -27,21 +17,6 @@ function App() {
         </a>
       </div>
       <h1>Vite + React</h1>
-      <WelcomeMessage />
-      <Header/>
-      <MainContent/>
-      <Footer /> 
-      <UserProfile 
-        name="Alice" 
-        age="25" 
-        bio="Loves hiking and photography" 
-      />
-      <Counter />
-       <UserContext.Provider value={userData}>
-      <ProfilePage />
-    </UserContext.Provider>
-
-
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
@@ -56,6 +31,5 @@ function App() {
     </>
   )
 }
-
 
 export default App
