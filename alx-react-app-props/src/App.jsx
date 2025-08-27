c7eb9c9a35c53ec8d14893dc4cd31fad1f2b4909
dwin504 <dwintez254@gmail.com>
2025-08-28 02:14:12 +0300

    added alx-react-app-props/src/App.jsx and installed zustand package

--- alx-react-app-props/src/App.jsx
+++ alx-react-app-props/src/App.jsx
@@ -6,8 +6,9 @@ import Header from './components/Header';
 import MainContent from './components/Maincontent';
 import Footer from './components/Footer';
 import UserProfile from './components/UserProfile';
-import ProfilePage from './ProfilePage';
-import { UserContext } from './UserContext'; 
+import ProfilePage from './components/ProfilePage'; 
+import { UserContext } from './components/UserContext';
+
 
 
 function App() {
