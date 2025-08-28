860ca94d4d3a704c98e9740aca8e7ac5b461e176
dwin504 <dwintez254@gmail.com>
2025-08-28 12:16:50 +0300

    added recipeStore.js file for managing recipes in the app

---
+++ recipe-sharing-app/src/recipeStore.js
@@ -0,0 +1,19 @@
+import { create } from 'zustand';
+
+export const useRecipeStore = create((set) => ({
+  recipes: [],
+  addRecipe: (recipe) =>
+    set((state) => ({
+      recipes: [...state.recipes, { ...recipe, id: Date.now() }],
+    })),
+  updateRecipe: (id, updatedRecipe) =>
+    set((state) => ({
+      recipes: state.recipes.map((recipe) =>
+        recipe.id === id ? { ...recipe, ...updatedRecipe } : recipe
+      ),
+    })),
+  deleteRecipe: (id) =>
+    set((state) => ({
+      recipes: state.recipes.filter((recipe) => recipe.id !== id),
+    })),
+}));
